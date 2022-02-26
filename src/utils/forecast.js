edc2bad85257f2b0e5ee7b1e13aa49396299ea0e
JT <jdmtran@gmail.com>
2022-02-25 16:12:10 -0800

    Add visibility weather info response

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,22 +11,22 @@ import request from 'request';
 
 export const forecast = (latitude, longitude, callback) => {
     let url = `http://api.weatherstack.com/current?access_key=0012c1b467eb5c43a9e7e9d769c744d6&query=${latitude},${longitude}&units=f`;
-    
+
     request({ url, json: true }, (error, response, body) => {
         if (error) {
-            callback ({errormsg: 'Unable to connect to the weather service'}, undefined);
+            callback({ errormsg: 'Unable to connect to the weather service' }, undefined);
         } else {
             if (response.body.error) {
-                callback ({errormsg: `Unable to find location due to error: ${response.body.error.info}.`}, undefined);
+                callback({ errormsg: `Unable to find location due to error: ${response.body.error.info}.` }, undefined);
             } else {
-                const {weather_descriptions, temperature, feelslike} = body.current;
+                const { weather_descriptions, temperature, feelslike, visibility } = body.current;
                 let desc;
                 if (weather_descriptions.length > 1) {
                     desc = weather_descriptions.join(' .');
                 } else {
                     desc = weather_descriptions.join(' .') + '.';
                 }
-                callback (undefined, `${desc} It is currently ${temperature} degress. It feels like ${feelslike} degrees.`)
+                callback(undefined, `${desc} It is currently ${temperature} degress. It feels like ${feelslike} degrees. Visibility is ${visibility}.`)
             }
         }
     })
